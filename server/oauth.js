8883d4012dbb868cc83c7c57a87da4cd21aff159
Siebe Baree <siebe.baree@outlook.com>
2022-10-23 19:54:59 +0200

    Initial commit

---
+++ server/oauth.js
@@ -0,0 +1,89 @@
+import { config } from "dotenv"
+if (process.env.NODE_ENV !== 'production') {
+    config();
+}
+
+const API_ENDPOINT = "https://discord.com/api/v10";
+const REDIRECT_URI = `http://localhost:5173/callback`;
+
+export async function exchangeCode(code) {
+    const res = await fetch(`${API_ENDPOINT}/oauth2/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        })
+    });
+    return await res.json();
+}
+
+export async function refreshToken(refreshToken) {
+    const res = await fetch(`${API_ENDPOINT}/oauth2/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken
+        })
+    });
+    return await res.json();
+}
+
+export async function getToken(token) {
+    const res = await fetch(`${API_ENDPOINT}/oauth2/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            'grant_type': 'client_credentials',
+            'scope': 'identify connections'
+        }),
+    });
+    return await res.json();
+}
+
+export async function revokeToken(token) {
+    const res = await fetch(`${API_ENDPOINT}/oauth2/token/revoke`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            token: token
+        })
+    });
+    return await res.json();
+}
+
+export async function getUser(token) {
+    const res = await fetch(`${API_ENDPOINT}/users/@me`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    return await res.json();
+}
+
+export async function getGuilds(token) {
+    const res = await fetch(`${API_ENDPOINT}/users/@me/guilds`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    return await res.json();
+}
