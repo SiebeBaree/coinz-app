b92a22f9dd96c1ac0ffda5b4d935738ee0d356a5
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 15:04:44 +0200

    Moved fetchData to helpers.js

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,32 +1,14 @@
 import { useEffect } from 'react';
-import { API_URI } from '../assets/data/config.json';
+import { getAccessToken, getUser } from './helpers';
 
 export default function Dashboard() {
     useEffect(() => {
-        async function fetchData(token) {
-            const res = await fetch(`${API_URI}/discord/user?code=${token}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return await res.json();
-        }
-
-        if (!sessionStorage.getItem('access_token')) {
+        if (!getAccessToken()) {
             sessionStorage.clear();
-            localStorage.clear();
             document.location.replace('/login');
         } else {
             if (!sessionStorage.getItem('user')) {
-                fetchData(sessionStorage.getItem('access_token')).then((data) => {
-                    sessionStorage.setItem('user', JSON.stringify(data));
-                    sessionStorage.setItem('user_id', data.id);
-                    sessionStorage.setItem('user_username', data.username);
-                    sessionStorage.setItem('user_discriminator', data.discriminator);
-                    sessionStorage.setItem('user_avatar', data.avatar);
-                    window.location.reload();
-                });
+                getUser().then(() => window.location.reload());
             }
         }
     })
