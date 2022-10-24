7550f45a50c12a7aa1b502e07dad4b23f49dbf6f
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 15:04:58 +0200

    Added support for refresh token

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { API_URI } from '../assets/data/config.json';
+import { getAccessToken, getRefreshToken } from './helpers';
 
 const DISCORD_CLIENT_ID = "938771676433362955";
 const API_ENDPOINT = "https://discord.com/api/v10";
@@ -8,8 +10,33 @@ const LOGIN_URI = `${API_ENDPOINT}/oauth2/authorize?client_id=${DISCORD_CLIENT_I
 
 export default function Login() {
     useEffect(() => {
-        if (sessionStorage.getItem('access_token')) {
+        async function fetchData(token) {
+            const res = await fetch(`${API_URI}/discord/refresh`, {
+                method: 'POST',
+                body: new URLSearchParams({
+                    token: token,
+                }),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            return await res.json();
+        }
+
+        const refreshToken = getRefreshToken();
+        if (getAccessToken() && parseInt(sessionStorage.getItem('expires_in')) > parseInt(Date.now() / 1000)) {
             document.location.replace('/dashboard');
+        } else if (refreshToken) {
+            fetchData(refreshToken).then((data) => {
+                if (!data.error) {
+                    sessionStorage.setItem('access_token', data.access_token);
+                    sessionStorage.setItem('expires_in', parseInt(Date.now() / 1000) + data.expires_in);
+                    localStorage.setItem('refresh_token', data.refresh_token);
+                    document.location.replace('/dashboard');
+                } else {
+                    document.location.replace(LOGIN_URI);
+                }
+            });
         } else {
             document.location.replace(LOGIN_URI);
         }
