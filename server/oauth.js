42977745fda990ac344658f794a575af58e76ba5
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 15:03:17 +0200

    Replaced fetch with axios

--- server/oauth.js
+++ server/oauth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { config } from "dotenv"
 if (process.env.NODE_ENV !== 'production') {
     config();
@@ -7,83 +8,39 @@ const API_ENDPOINT = "https://discord.com/api/v10";
 const REDIRECT_URI = process.env.WEBAPP_URL + "/callback";
 
 export async function exchangeCode(code) {
-    const res = await fetch(`${API_ENDPOINT}/oauth2/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            client_id: process.env.DISCORD_CLIENT_ID,
-            client_secret: process.env.DISCORD_CLIENT_SECRET,
-            grant_type: 'authorization_code',
-            code: code,
-            redirect_uri: REDIRECT_URI
-        })
-    });
-    return await res.json();
+    const res = await axios.post(`${API_ENDPOINT}/oauth2/token`, new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+    }));
+    return res.data;
 }
 
 export async function refreshToken(refreshToken) {
-    const res = await fetch(`${API_ENDPOINT}/oauth2/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            client_id: process.env.DISCORD_CLIENT_ID,
-            client_secret: process.env.DISCORD_CLIENT_SECRET,
-            grant_type: 'refresh_token',
-            refresh_token: refreshToken
-        })
-    });
-    return await res.json();
+    const res = await axios.post(`${API_ENDPOINT}/oauth2/token`, new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+    }));
+    return res.data;
 }
 
 export async function getToken() {
-    const res = await fetch(`${API_ENDPOINT}/oauth2/token`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            'grant_type': 'client_credentials',
-            'scope': 'identify connections'
-        }),
-    });
-    return await res.json();
+    const res = await axios.post(`${API_ENDPOINT}/oauth2/token`, new URLSearchParams({
+        'grant_type': 'client_credentials',
+        'scope': 'identify connections'
+    }));
+    return res.data;
 }
 
 export async function revokeToken(token) {
-    const res = await fetch(`${API_ENDPOINT}/oauth2/token/revoke`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-            client_id: process.env.DISCORD_CLIENT_ID,
-            client_secret: process.env.DISCORD_CLIENT_SECRET,
-            token: token
-        })
-    });
-    return await res.json();
-}
-
-export async function getUser(token) {
-    const res = await fetch(`${API_ENDPOINT}/users/@me`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    return await res.json();
-}
-
-export async function getGuilds(token) {
-    const res = await fetch(`${API_ENDPOINT}/users/@me/guilds`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    return await res.json();
+    const res = await axios.post(`${API_ENDPOINT}/oauth2/token/revoke`, new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        token: token
+    }));
+    return { status: res.status };
 }
