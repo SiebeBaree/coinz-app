10ce7a3d6c7129bf48ae7efaeb843a5771c01464
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 11:53:34 +0200

    Fixed links

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -1,4 +1,5 @@
 import '../style/Navbar.css'
+import { Link } from 'react-router-dom'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -14,13 +15,13 @@ function FooterComponent() {
 
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="unimportant-content">
-                            <Nav.Link href="/store">Store</Nav.Link>
-                            <Nav.Link href="/discord">Support Server</Nav.Link>
-                            <Nav.Link href="/invite">Invite</Nav.Link>
+                            <Nav.Link as={Link} to="/store">Store</Nav.Link>
+                            <Nav.Link as={Link} to="/discord">Support Server</Nav.Link>
+                            <Nav.Link as={Link} to="/invite">Invite</Nav.Link>
                         </Nav>
                         <Nav className="ms-auto">
-                            <Nav.Link href="/tos">Terms of Service</Nav.Link>
-                            <Nav.Link href="/privacy-policy">Privacy Policy</Nav.Link>
+                            <Nav.Link as={Link} to="/tos">Terms of Service</Nav.Link>
+                            <Nav.Link as={Link} to="/privacy">Privacy Policy</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
