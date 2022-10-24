ea626d6f3f149aabfc96c75c8d568dc951da9aeb
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 16:17:50 +0200

    Added mongodb support

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
-// Environment Variables (Stripe API Key)
+import mongoose from 'mongoose'
 import { config } from "dotenv"
-if (process.env.NODE_ENV !== 'production') {
-    config();
-}
+config();
 
 // Initialize Stripe
 // import Stripe from 'stripe';
 // export const stripe = new Stripe(process.env.STRIPE_SECRET, {
 //     apiVersion: '2022-08-01',
 // });
+
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URI, {
+    dbName: process.env.NODE_ENV === 'production' ? 'coinz' : 'coinz_beta',
+    useNewUrlParser: true,
+    maxPoolSize: 100,
+    minPoolSize: 5,
+    family: 4,
+    heartbeatFrequencyMS: 30000,
+    keepAlive: true,
+    keepAliveInitialDelay: 300000
+}).then(() => console.log('Connected to MongoDB'));
 
 // Start the API with Express
 import { app } from './routes.js';
