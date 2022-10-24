13c73864519e1b4ce79c1abf4ee9838ffeea797a
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 15:59:42 +0200

    Added state parameter for security

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,10 @@
 import { useEffect } from 'react';
-import { API_URI } from '../assets/data/config.json';
-import { getAccessToken, getRefreshToken } from './helpers';
-
-const DISCORD_CLIENT_ID = "938771676433362955";
-const API_ENDPOINT = "https://discord.com/api/v10";
-const REDIRECT_URI = "http://localhost:5173/callback";
-const SCOPE = ['identify', 'guilds'];
-const LOGIN_URI = `${API_ENDPOINT}/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE.join('%20')}&prompt=consent`;
+import { API_URI, API_ENDPOINT, DISCORD_CLIENT_ID, SCOPE } from '../assets/data/config.json';
+import { generateRandomString, getAccessToken, getRefreshToken, setTokenItems } from './helpers';
 
 export default function Login() {
+    const randomString = generateRandomString();
+
     useEffect(() => {
         async function fetchData(token) {
             const res = await fetch(`${API_URI}/discord/refresh`, {
@@ -23,15 +19,17 @@ export default function Login() {
             return await res.json();
         }
 
+        localStorage.setItem('oauth-state', randomString);
+        const LOGIN_URI = `${API_ENDPOINT}/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${window.location.origin + "/callback"}` +
+            `&response_type=code&scope=${SCOPE.join('%20')}&prompt=consent&state=${encodeURIComponent(window.btoa(randomString))}`;
+
         const refreshToken = getRefreshToken();
         if (getAccessToken() && parseInt(sessionStorage.getItem('expires_in')) > parseInt(Date.now() / 1000)) {
             document.location.replace('/dashboard');
         } else if (refreshToken) {
             fetchData(refreshToken).then((data) => {
                 if (!data.error) {
-                    sessionStorage.setItem('access_token', data.access_token);
-                    sessionStorage.setItem('expires_in', parseInt(Date.now() / 1000) + data.expires_in);
-                    localStorage.setItem('refresh_token', data.refresh_token);
+                    setTokenItems(data);
                     document.location.replace('/dashboard');
                 } else {
                     document.location.replace(LOGIN_URI);
