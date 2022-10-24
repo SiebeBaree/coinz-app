cbb1a1b7aebe0c18c3511e58788ef38b1a8aeb58
Siebe Baree <siebe.baree@outlook.com>
2022-10-24 15:59:57 +0200

    Moved code to helpers.js

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,32 +1,9 @@
 import { useEffect } from 'react';
-import { API_URI } from '../assets/data/config.json';
+import { revokeToken } from './helpers';
 
 export default function Logout() {
     useEffect(() => {
-        async function fetchData(token) {
-            const res = await fetch(`${API_URI}/discord/revoke`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    code: token
-                })
-            });
-            return await res.json();
-        }
-
-        if (sessionStorage.getItem('access_token')) {
-            fetchData(sessionStorage.getItem('access_token')).then(() => {
-                sessionStorage.clear();
-                localStorage.clear();
-                document.location.replace('/');
-            });
-        } else {
-            sessionStorage.clear();
-            localStorage.clear();
-            document.location.replace('/');
-        }
+        revokeToken();
     }, [])
 
     return (<></>)
