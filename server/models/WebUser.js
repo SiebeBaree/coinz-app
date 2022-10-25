459fca94722e91cf47d1e424fdca057862b2b1b3
Siebe Baree <siebe.baree@outlook.com>
2022-10-25 17:18:05 +0200

    Removed file

--- server/models/WebUser.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const WebUser = mongoose.Schema({
-    token: { type: String, required: true, unique: true, index: true },
-    id: { type: String, required: true, index: true },
-    expireAt: { type: Number, default: Date.now() + 604800 }
-});
-
-export default mongoose.model('WebUser', WebUser, 'webusers');
