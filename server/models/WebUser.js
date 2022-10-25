29631d222b70c9f32511feb64311342deed69153
Siebe Baree <siebe.baree@outlook.com>
2022-10-25 10:57:00 +0200

    Added expireAt field

--- server/models/WebUser.js
+++ server/models/WebUser.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const WebUser = mongoose.Schema({
     token: { type: String, required: true, unique: true, index: true },
     id: { type: String, required: true, index: true },
-    createdAt: { type: Date, default: Date.now, expires: 604800 }
+    expireAt: { type: Number, default: Date.now() + 604800 }
 });
 
 export default mongoose.model('WebUser', WebUser, 'webusers');
