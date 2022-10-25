1638da6f0c3179c6814299c581783ae4463561b0
Siebe Baree <siebe.baree@outlook.com>
2022-10-25 20:45:34 +0200

    Updated Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <nav className="footer">FOOTER</nav>
+        <nav className="footer">FOOTER for Coinz</nav>
     )
 }
