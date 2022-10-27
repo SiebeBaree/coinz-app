53c3827e7e722a20841511ae33fd9b3348f263f8
Siebe Baree <siebe.baree@outlook.com>
2022-10-27 14:13:12 +0200

    Initial commit

---
+++ lib/types.ts
@@ -0,0 +1,26 @@
+export interface Token {
+    access_token: string;
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
+    token_type: string;
+    error?: string
+}
+
+export interface User {
+    id: string;
+    username: string;
+    discriminator: string;
+    avatar: string;
+    bot?: boolean;
+    system?: boolean;
+    mfa_enabled?: boolean;
+    banner?: string;
+    accent_color?: number;
+    locale?: string;
+    verified?: boolean;
+    email?: string;
+    flags?: number;
+    premium_type?: number;
+    public_flags?: number;
+}
