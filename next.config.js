4768a82b9ea94ef188a847c2bc82929081e420b4
Siebe Baree <siebe.baree@outlook.com>
2022-10-27 12:22:06 +0200

    Added image host

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/emojis/**',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
