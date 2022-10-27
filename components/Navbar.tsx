7b5970518c27e3882a433336947b12ed71e47740
Siebe Baree <siebe.baree@outlook.com>
2022-10-27 12:23:26 +0200

    Updated navbar to nextjs 13

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/Navbar.module.css'
+import styles from '../styles/navbar.module.css'
 import Link from 'next/link'
 
 const DISCORD_BASE_CDN = 'https://cdn.discordapp.com/';
@@ -10,7 +10,7 @@ export default function NavbarComponent() {
         <div className='container'>
             <nav className={`navbar navbar-expand-lg ${styles.navbarHeader} ${styles.borderUnder}`}>
                 <div className='container-fluid'>
-                    <Link href="/"><a className={styles.navbarBrand}>Coinz</a></Link>
+                    <Link href="/" className={styles.navbarBrand}>Coinz</Link>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span className="navbar-toggler-icon"></span>
@@ -24,24 +24,24 @@ export default function NavbarComponent() {
                         <div className="offcanvas-body">
                             <ul className="navbar-nav">
                                 <li className="nav-item">
-                                    <Link href="/faq"><a className={`nav-link ${styles.navbarLink}`} aria-current="page">FAQ</a></Link>
+                                    <Link href="/faq" className={`nav-link ${styles.navbarLink}`}>FAQ</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link href="/commands"><a className={`nav-link ${styles.navbarLink}`}>Commands</a></Link>
+                                    <Link href="/commands" className={`nav-link ${styles.navbarLink}`}>Commands</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link href="/store"><a className={`nav-link ${styles.navbarLink}`}>Store</a></Link>
+                                    <Link href="/store" className={`nav-link ${styles.navbarLink}`}>Store</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link href="/invite"><a className={`nav-link ${styles.navbarLink}`}>Invite</a></Link>
+                                    <Link href="/invite" className={`nav-link ${styles.navbarLink}`} target="_blank">Invite</Link>
                                 </li>
                             </ul>
                             <ul className="navbar-nav ms-auto">
                                 <li className='nav-item'>
-                                    <Link href="/store"><a className={`nav-link ${styles.navbarLink}`}>Support</a></Link>
+                                    <Link href="/discord" className={`nav-link ${styles.navbarLink}`} target="_blank">Support</Link>
                                 </li>
                                 <li className='nav-item'>
-                                    <Link href="/invite"><a className={`nav-link ${styles.navbarLink}`}>Login Via Discord</a></Link>
+                                    <Link href="/login" className={`nav-link ${styles.navbarLink}`}>Login Via Discord</Link>
                                 </li>
                             </ul>
                         </div>
