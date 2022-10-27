62bcd41e68ef100d2b4c433de6a508430f22990b
Siebe Baree <siebe.baree@outlook.com>
2022-10-27 14:13:28 +0200

    Fixed bug

--- pages/invite.tsx
+++ pages/invite.tsx
@@ -3,11 +3,12 @@ import { useRouter } from 'next/router'
 import Redirect from '../components/Redirect'
 
 export default function Invite() {
-    const { query } = useRouter();
+    const router = useRouter();
 
     useEffect(() => {
-        window.location.replace(`https://discord.com/api/oauth2/authorize?client_id=938771676433362955&permissions=313344&scope=bot%20applications.commands${query.id === undefined ? "" : `&guild_id=${id}`}`)
-    })
+        if (!router.isReady) return;
+        window.location.replace(`https://discord.com/api/oauth2/authorize?client_id=938771676433362955&permissions=313344&scope=bot%20applications.commands${router.query.id === undefined ? "" : `&guild_id=${router.query.id}`}`)
+    }, [router.isReady]);
 
     return (
         <Redirect />
