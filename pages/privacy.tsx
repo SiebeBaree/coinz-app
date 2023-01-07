d2e78a42e0d9df4f499a1a2e6beae7d8e0bba775
Siebe Baree <siebe.baree@outlook.com>
2023-01-07 17:12:02 +0100

    Updated privacy policy

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -9,78 +9,93 @@ export default function PrivacyPolicy() {
         <div className="container mt-5 mb-5 text-white">
             <h1 className="text-center">Privacy Policy for Coinz</h1>
 
-            <p>At Coinzbot, accessible from the <Link href="/" className='text-white text-decoration-none'>coinzbot website</Link>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Coinzbot and how we use it.</p>
+            <p>One of our main priorities is the privacy of our users. This privacy policy (&quot;Privacy Policy&quot;) explains how we collect, use, and share information when you access or use our Discord bot or website. By accessing or using Coinz, you agree to the collection, use, and sharing of your information as described in this Privacy Policy.</p>
 
-            <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
-
-            <h3>General Data Protection Regulation (GDPR)</h3>
-            <p>We are a Data Controller of your information.</p>
-
-            <p>Coinz legal basis for collecting and using the personal information described in this Privacy Policy depends on the Personal Information we collect and the specific context in which we collect the information:</p>
+            <h4>I. Information We Collect</h4>
+            <p>We collect information you provide directly to us when you access or use Coinz. This includes:</p>
             <ul>
-                <li>Coinz needs to perform a contract with you</li>
-                <li>You have given Coinz permission to do so</li>
-                <li>Processing your personal information is in Coinz legitimate interests</li>
-                <li>Coinz needs to comply with the law</li>
+                <li>Your Discord ID</li>
+                <li>Your Stripe customer ID if you purchase Premium features or lootboxes on our website</li>
             </ul>
 
-            <p>Coinz will retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.</p>
-
-            <p>If you are a resident of the European Economic Area (EEA), you have certain data protection rights. If you wish to be informed what Personal Information we hold about you and if you want it to be removed from our systems, please contact us.</p>
-
-            <p>In certain circumstances, you have the following data protection rights:</p>
+            <h4>II. How We Use Your Information</h4>
+            <p>We use the information we collect to provide, maintain, and improve Coinz. This includes:</p>
             <ul>
-                <li>The right to access, update or to delete the information we have on you.</li>
-                <li>The right of rectification.</li>
-                <li>The right to object.</li>
-                <li>The right of restriction.</li>
-                <li>The right to data portability</li>
-                <li>The right to withdraw consent</li>
+                <li>Processing payments for Premium features or lootboxes</li>
+                <li>Customizing your experience on Coinz</li>
+                <li>Analyzing and improving the performance of Coinz</li>
             </ul>
 
-            <h3>Log Files</h3>
+            <h4>III. Legal Basis for Processing</h4>
+            <p>We process your personal information in accordance with the General Data Protection Regulation (GDPR). Our legal basis for processing your personal information is as follows:</p>
+            <ul>
+                <li>Performance of a contract: We process your personal information to provide Coinz to you and fulfill our contractual obligations to you.</li>
+                <li>Legitimate interests: We process your personal information for our legitimate interests in operating and improving Coinz.</li>
+                <li>Consent: We may ask for your consent to process certain personal information, such as marketing communications.</li>
+            </ul>
 
-            <p>Coinzbot follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services&apos; analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet
-                Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the
-                website, and gathering demographic information.</p>
+            <h4>IV. Sharing Your Information</h4>
+            <p>We do not sell or rent your information to third parties. We may share your information with third parties in the following circumstances:</p>
+            <ul>
+                <li>With service providers who assist us in providing Coinz</li>
+                <li>To comply with legal obligations, such as responding to a subpoena or court order</li>
+                <li>To protect the rights, property, or safety of Coinz, our users, or the public</li>
+            </ul>
 
-            <h3>Cookies and Web Beacons</h3>
+            <h4>V. Data Retention</h4>
+            <p>We retain the information we collect for as long as it is necessary to provide Coinz and to comply with legal obligations.</p>
 
-            <p>Like any other website, Coinzbot uses &apos;cookies&apos;. These cookies are used to store information including visitors&apos; preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users&apos; experience by customizing our web page content
-                based on visitors&apos; browser type and/or other information.</p>
+            <h4>VI. Your Rights</h4>
+            <p>Under the GDPR, you have the following rights regarding your personal information:</p>
 
+            <ul>
+                <li>Right of access: You have the right to request access to your personal information and any information we hold about you.</li>
+                <li>Right to rectification: You have the right to request that we correct any inaccurate personal information we hold about you.</li>
+                <li>Right to erasure: You have the right to request that we delete your personal information.</li>
+                <li>Right to restrict processing: You have the right to request that we restrict the processing of your personal information.</li>
+                <li>Right to object: You have the right to object to the processing of your personal information.</li>
+                <li>Right to data portability: You have the right to request that we transfer your personal information to another organization.</li>
+            </ul>
+            <p>You may exercise these rights by contacting us in our <Link href="/discord" className='text-primary'>Discord Server</Link>.</p>
 
-            <h3>Privacy Policies</h3>
+            <h4>VII. Your Choices</h4>
+            <p>You have the following choices regarding the information we collect and how we use it:</p>
 
-            <p>You may consult this list to find the Privacy Policy for each of the advertising partners of Coinzbot.</p>
+            <ul>
+                <li>You may opt out of marketing communications from Coinz by following the unsubscribe instructions in the communication</li>
+                <li>You may access, modify, or delete your personal information by contacting us in our <Link href="/discord" className='text-primary'>Discord Server</Link></li>
+            </ul>
 
-            <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Coinzbot, which are sent directly to users&apos; browser. They automatically receive your IP address when this occurs.
-                These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+            <h4>VIII. Children&apos;s Privacy</h4>
+            <p>Coinz is not intended for children under the age of 13. We do not knowingly collect personal information from children under 13. If we learn that we have collected personal information from a child under 13, we will delete it as soon as possible.</p>
 
-            <p>Note that Coinzbot has no access to or control over these cookies that are used by third-party advertisers.</p>
+            <h4>IX. Changes to This Privacy Policy</h4>
+            <p>We may update this Privacy Policy from time to time. We will post any changes on this page and encourage you to review this Privacy Policy regularly. Your continued access or use of Coinz constitutes your acceptance of any changes to this Privacy Policy.</p>
 
-            <h3>Third Party Privacy Policies</h3>
+            <h4>X. Cookies</h4>
+            <p>We use cookies and similar technologies, such as web beacons, to collect and use information when you access or use Coinz. Cookies are small data files that are stored on your device and used to track your activity on the internet.</p>
+            <p>We use cookies to:</p>
+            <ul>
+                <li>Personalize your experience on Coinz</li>
+                <li>Analyze and improve the performance of Coinz</li>
+                <li>Remember your preferences</li>
+            </ul>
 
-            <p>Coinzbot&apos;s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain
+            <p>You can control the use of cookies by adjusting your browser settings. Please note that disabling cookies may affect your ability to use certain features of Coinz.</p>
+            <p>By accessing or using Coinz, you consent to the use of cookies as described in this Privacy Policy.</p>
+
+            <h4>XI. Third Party Privacy Policies</h4>
+
+            <p>The Privacy Policy of Coinz does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain
                 options. </p>
 
             <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites.</p>
 
-            <h3>Children&apos;s Information</h3>
+            <h4>XII. Consent</h4>
+            <p>By using our website and discord bot, you hereby consent to our Privacy Policy and agree to its terms.</p>
 
-            <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
-
-            <p>Coinzbot does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly
-                remove such information from our records.</p>
-
-            <h3>Online Privacy Policy Only</h3>
-
-            <p>Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Coinzbot. This policy is not applicable to any information collected offline or via
-                channels other than this website.</p>
-
-            <h3>Consent</h3>
-
-            <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
+            <h4>XIII. Contact Us</h4>
+            <p>If you have any questions or concerns about our collection, use, or sharing of your information, please contact us in our <Link href="/discord" className='text-primary'>Discord Server</Link>.</p>
         </div >
     );
 }
