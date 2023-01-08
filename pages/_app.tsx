45d04e78986ab8c5b5eae78660e8943abdb1a74f
Siebe Baree <siebe.baree@outlook.com>
2023-01-08 18:49:15 +0100

    Fixed website embed on discord

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,13 +33,12 @@ function MyApp({ Component, pageProps }) {
                 <meta property="og:image" content="/logo512.png" />
                 <meta property="og:description"
                     content="Coinz is the only discord economy bot you will ever need. It has 45+ commands to keep you entertained." />
-                <meta property="og:title" content="SiebeBaree" />
+                <meta property="og:title" content="Coinz" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://coinzbot.xyz/" />
                 <meta property="og:locale" content="en_US" />
 
                 <meta name="twitter:card" content="summary" />
-                <meta name="twitter:site" content="@BareeSiebe" />
                 <meta name="twitter:creator" content="@BareeSiebe" />
                 <meta name="twitter:title" content="Coinz" />
                 <meta name="twitter:description"
