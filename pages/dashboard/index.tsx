d3c6567687e78e59ade8edf8c4a96dd0e7f41b1e
Siebe Baree <siebe.baree@outlook.com>
2023-01-08 18:49:02 +0100

    Removed old discord OAuth2

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from 'react';
-import { getAccessToken } from '../../lib/storage';
-import { getUser } from '../../lib/api';
-import { User } from '../../lib/types';
+import { GetServerSidePropsContext } from 'next';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+}
 
 export default function Dashboard() {
-    useEffect(() => {
-        if (!getAccessToken()) {
-            sessionStorage.clear();
-            document.location.replace('/login');
-        } else {
-            getUser().then((user: User) => {
-                window.location.replace('/dashboard/' + user.id);
-            });
-        }
-    });
-
-    return (<></>);
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,49 +1,14 @@
 import { useEffect } from 'react';
-import config from '../lib/data/config.json';
-const { API_ENDPOINT, DISCORD_CLIENT_ID, SCOPE } = config;
-
-import { discordRefreshToken, discordRevokeToken } from '../lib/api';
-import { getRefreshToken, getAccessToken, setTokenItems } from '../lib/storage';
-import { Token } from '../lib/types';
-
-const generateRandomString = (length = 0) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    length = length === 0 ? Math.floor(Math.random() * (48 - 24 + 1) + 24) : length;
-
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-    return result;
-};
-
-const setOauthState = () => {
-    const state = generateRandomString();
-    localStorage.setItem('oauth_state', state);
-    return `${API_ENDPOINT}/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${window.location.origin + '/callback'}` +
-        `&response_type=code&scope=${SCOPE.join('%20')}&prompt=consent&state=${encodeURIComponent(state)}`;
-};
 
 export default function Login() {
     useEffect(() => {
-        const refreshToken = getRefreshToken();
-        if (getAccessToken() && parseInt(sessionStorage.getItem('expires_in')) > Math.floor(Date.now() / 1000)) {
-            document.location.replace('/dashboard');
-        } else if (refreshToken) {
-            discordRefreshToken(refreshToken).then((data: Token) => {
-                if (!data.error) {
-                    setTokenItems(data);
-                    document.location.replace('/dashboard');
-                } else {
-                    document.location.replace(setOauthState());
-                }
-            }).catch(() => {
-                discordRevokeToken().then(() => document.location.replace(setOauthState()));
+        fetch('http://localhost:3001/api/discord/login', {
+            method: 'GET',
+            mode: 'no-cors',
+        })
+            .then((res) => {
+                console.log(res);
             });
-        } else {
-            document.location.replace(setOauthState());
-        }
     });
 
     return (<></>);
