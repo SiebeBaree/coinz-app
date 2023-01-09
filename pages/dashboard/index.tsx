1a04455e4762dd5a0e0f51c95ab1c74285b48126
Siebe Baree <siebe.baree@outlook.com>
2023-01-09 19:23:57 +0100

    Updated to support new login system

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,25 @@
 import { GetServerSidePropsContext } from 'next';
+import { useEffect } from 'react';
+import { fetchUser } from '../../lib/api';
+import { User } from '../../lib/types';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+    return await fetchUser(context);
 }
 
-export default function Dashboard() {
+export default function Dashboard({ user }: { user: User }) {
+    useEffect(() => {
+        sessionStorage.setItem('user_id', user.id);
+        sessionStorage.setItem('user_username', user.username);
+        sessionStorage.setItem('user_discriminator', user.discriminator);
+        sessionStorage.setItem('user_avatar', user.avatar);
+    });
+
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <p>Welcome {user.username}#{user.discriminator}!</p>
+            <p>{user.id}</p>
+        </>
+    );
 }
