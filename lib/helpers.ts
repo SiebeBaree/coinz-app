e5a8295898ddb981a239d6d3dfd5fd3940825ce5
Siebe Baree <siebe.baree@outlook.com>
2023-01-09 19:24:58 +0100

    Initial commit

---
+++ lib/helpers.ts
@@ -0,0 +1,8 @@
+import { GetServerSidePropsContext } from 'next';
+
+export const validateCookies = (ctx: GetServerSidePropsContext) => {
+    const sessionId = ctx.req.cookies['connect.sid'];
+    return sessionId ? ({
+        Cookie: `connect.sid=${sessionId}`,
+    }) : false;
+};
