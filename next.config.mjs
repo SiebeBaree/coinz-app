ee1d501717de316c69317742cb93c4a62f415df0
Siebe Baree <siebe.baree@outlook.com>
2023-02-10 12:44:55 +0100

    Fixed linting

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        pathname: '/emojis/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        pathname: '/avatars/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        pathname: '/embed/avatars/**',
-      },
-    ],
-  }
-}
+    output: 'standalone',
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.discordapp.com',
+                pathname: '/emojis/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.discordapp.com',
+                pathname: '/avatars/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.discordapp.com',
+                pathname: '/embed/avatars/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
