e39c99a899bf06726ca6ff03492f5c55284d12d5
Siebe Baree <siebe.baree@outlook.com>
2023-04-26 23:05:06 +0200

    Added shard and cluster types

--- lib/types.ts
+++ lib/types.ts
@@ -10,3 +10,14 @@ export type Item = {
     multiplier?: number;
     duration?: number;
 }
+
+export type Shard = {
+    id: number;
+    ping: number;
+    guildcount: number;
+}
+
+export type Cluster = {
+    id: number;
+    shards: Shard[];
+}
