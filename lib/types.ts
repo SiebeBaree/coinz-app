9013cd797a7d0b66c67871ca5e93d660dabc4a29
Siebe Baree <siebe.baree@outlook.com>
2023-05-01 16:55:33 +0200

    Removed Item

--- lib/types.ts
+++ lib/types.ts
@@ -1,16 +1,3 @@
-export type Item = {
-    itemId: string;
-    category: string;
-    name: string;
-    emoteId: string;
-    description: string;
-    longDescription?: string;
-    buyPrice?: number;
-    sellPrice?: number;
-    multiplier?: number;
-    duration?: number;
-}
-
 export type Shard = {
     id: number;
     ping: number;
